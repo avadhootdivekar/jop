9eb24980a31fa4a2904e707591eb870bd7dfef15
Avadhoot Divekar <avadhoot@startuppartner.co.in>
2022-09-28 11:23:52 +0000

    WIP

--- antlr/test_1_visitor.py
+++ antlr/test_1_visitor.py
@@ -1,3 +1,5 @@
+from curses import tigetflag
+from http.client import RESET_CONTENT
 from mimetypes import common_types
 from statistics import variance
 from syslog import LOG_ERR
@@ -28,7 +30,9 @@ gVarMap={}
 gFunMap={}
 ASSIGNMENT          = "assign"
 DEFAULT             = "default"
+PTYPE_DEFAULT       = "default"
 PTYPE_MEMBER        = "member"
+PTYPE_GET_TREE      = "get_tree"
 
 def logFallBack(level=0, str1="" , tid="000OO"):
     print( "<< check "+str(tid) + " >> " + str1)
@@ -149,21 +153,26 @@ class customListener(test_1Listener):
 class customVisitor(test_1Visitor):
     def visitMatch_b(self, ctx: test_1Parser.Match_bContext , parent_type=DEFAULT):
         self.commonVisitor(ctx , "match_b")
-        return super().visitMatch_b(ctx) 
+        return RET_SUCCESS, super().visitMatch_b(ctx) 
 
     def visitMath_b_op(self, ctx: test_1Parser.Math_b_opContext):
         val = ""
+        retCode = RET_FAILURE
         if ctx.P() != None:
             val = "+"
+            retCode = RET_SUCCESS
         elif ctx.N() != None:
             val = "-"
+            retCode = RET_SUCCESS
         elif ctx.M() != None:
             val = "*"
+            retCode = RET_SUCCESS
         elif ctx.D() != None:
             val = "/"
+            retCode = RET_SUCCESS
         if val =="" :
             log(LOG_WARNING , "Unexpected binary operator. " , tid="100af")
-        return val
+        return retCode, val
     
     def visitLines(self, ctx: test_1Parser.LinesContext , parent_type=DEFAULT):
         self.commonVisitor(ctx , "Lines")
@@ -175,6 +184,7 @@ class customVisitor(test_1Visitor):
         self.commonVisitor(ctx , "LIST_")
         v = []
         count = ctx.getChildCount()
+        retCode = RET_FAILURE
         # log(LOG_DEBUG , "list tokens : " + str(ctx.getTokens()))
         for i in range(count-1):
             if i==0:
@@ -183,44 +193,47 @@ class customVisitor(test_1Visitor):
             node = ctx.getChild(i)
             log(LOG_DEBUG , "list node : " + node.getText())
             if (node.accept(self) != None):
-                v.append(node.accept(self))
+                v.append(node.accept(self)[1])
         log(LOG_DEBUG , "Final derived list : " + str(v))
-        return v
+        return RET_SUCCESS , v
 
     def visitCurly(self, ctx: test_1Parser.CurlyContext):
         self.commonVisitor(ctx , "  CURLY : ")
         v = {}
+        retCode = RET_FAILURE
         if ctx.curly() != None :
             for i in range(len(ctx.curly())):
                 log(LOG_ERROR , "Getting curly " + str(i))
-                v = v | ctx.curly(i).accept(self)
+                retCode , val = ctx.curly(i).accept(self)
+                v = v | val
         if ctx.pair() != None : 
             for i in range(len(ctx.pair())) : 
                 log(LOG_ERROR , "Getting pair " + str(i) )
-                temp_k , temp_v = ctx.pair(i).accept(self)
+                retCode , temp_k , temp_v = ctx.pair(i).accept(self)
                 v = v | {temp_k : temp_v}
         log(LOG_ERROR , "value for curly : " + str(v)  + " , where actual text : " + ctx.getText())
-        return v
+        return RET_SUCCESS, v
 
     def visitPair(self, ctx: test_1Parser.PairContext):
         self.commonVisitor(ctx, " PAIR : ")
         k = None
         v = None
+        retCode = RET_FAILURE
         if ctx.uid(0) == None:
             log(LOG_ERROR , "UID not found in pair.")
-        k = ctx.uid(0).accept(self)
+        retCode , k = ctx.uid(0).accept(self)
 
         if (ctx.uid(1) != None):
             log(LOG_DEBUG , "uid value for pair")
-            v = ctx.uid(1).accept(self)
+            retCode , v = ctx.uid(1).accept(self)
         elif (ctx.list_() != None ) :
             log(LOG_DEBUG , "curly value for pair")
-            v = ctx.list_().accept(self)
+            retCode , v = ctx.list_().accept(self)
         elif (ctx.curly() != None ) :
             log(LOG_DEBUG , "list value for pair")
-            v = ctx.curly().accept(self)
+            retCode , v = ctx.curly().accept(self)
         log(LOG_ERROR , "pair key : " + str(k) + " , v : " + str(v) )
-        return k , v
+        return RET_SUCCESS, k , v
 
     def visitUid(self, ctx: test_1Parser.UidContext):
         self.commonVisitor(ctx , "UID")
@@ -249,9 +262,9 @@ class customVisitor(test_1Visitor):
         if varName in gVarMap:
             print("Assignment variable already declared.")
         try:
-            value = ctx.rvalue().accept(self)
+            retCode , value = ctx.rvalue().accept(self)
             print("Derived rvalue : " + str(value) + " , for string : " + str(ctx.rvalue().getText()))
-            gVarMap[varName] = ctx.rvalue().accept(self)
+            retCode, gVarMap[varName] = ctx.rvalue().accept(self)
         except:
         	print("Failed..")
         # for i in range(0,count):
@@ -270,6 +283,8 @@ class customVisitor(test_1Visitor):
         '''
         v = None
         first = ctx.ID()
+        self.commonVisitor(ctx , "member")
+        log(LOG_DEBUG , "" , tid="100ar")
         if  first != None:
             if parent_type == PTYPE_MEMBER:
                 if str(first) in parent:
@@ -285,7 +300,9 @@ class customVisitor(test_1Visitor):
                 log(LOG_ERROR , "Unknown parent type. Malformed membership.")
         parent = v
         if ctx.member_candidate() != None :
+            log(LOG_DEBUG, "parent : "+ str(parent) , tid="100at" )
             success , v = self.evalMember(ctx.member_candidate(0) , memberList=ctx.member_candidate() , parent=parent , depth=0)
+            log(LOG_DEBUG , " success : "+str(success)+", v : " + str(v)  + "parent : " + str(parent) , tid="100aq")
             # for i in range(len(ctx.member_candidate())):
             #     cand = ctx.member_candidate(i)
             #     log(LOG_DEBUG , "member candidate ::" +str(i) +" ::" + str(ctx.member_candidate(i).getText()) )
@@ -294,38 +311,36 @@ class customVisitor(test_1Visitor):
             v = self.visitMember(ctx.member() , parent_type=PTYPE_MEMBER , parent = parent)
         else : 
             log(LOG_ERROR , "No member OR member candidate!!")
-        return v
+        log(LOG_DEBUG , "Member v : "+str(v) , tid="100ap"  )
+        return RET_SUCCESS, v
 
 
     def visitMember_candidate(self, ctx: test_1Parser.Member_candidateContext , parent_type=DEFAULT , parent={}):
         var = None
         success = False
         retCode = RET_FAILURE;
+        self.commonVisitor(ctx , "member candidate")
         if ctx.uid() != None:
             log(LOG_ERROR , "In member candidate , var : " + str(var)  + " , parent : " + str(parent) + " type : " +str(type(parent)) +", uid : " + str(ctx.uid().getText()) )
-            retCode, varName = ctx.uid().accept(self)
-            if (parent_type == DEFAULT) : 
-                log(LOG_ERROR , "Unexpected parent type , malformed membership.")
-            elif (parent_type == PTYPE_MEMBER ):
-                if ctx.uid().id_() != None :
-                    present , var = getVar(varName)
-                    k = str(var)
-                    if not present:
-                        log(LOG_ERROR , "Exception !! Member not present.")
-                        return False, {}
-                elif (ctx.uid().strings() != None ):
-                    k = ctx.uid().strings().accept(self)
-                log(LOG_DEBUG , "k : " +k  + " , type : " + str(type(k))  , tid="809kn")
-                if k in parent :
-                    var = parent[k]
-                    success = True
-                    log(LOG_DEBUG , "var : " + str(var) , tid="369ho")
-                else : 
-                    log(LOG_ERROR , "ERR! var : " +  str(var) + " , parent : " +str(parent)   , tid="400ij")
-                    success = False
+            retCode, val= ctx.uid().accept(self)
+
+            if type(val) == dict : 
+                log(LOG_WARNING , "Dictionary specified as member.. " , tid="100am")
+            k = val
+            log(LOG_DEBUG , "k : " +k  + " , type : " + str(type(k))  , tid="809kn")
+            if k in parent :
+                var = parent[k]
+                success = True
+                log(LOG_DEBUG , "var : " + str(var) , tid="369ho")
+            else : 
+                log(LOG_ERROR , "ERR! var : " +  str(var) + " , parent : " +str(parent)   , tid="400ij")
+                success = False
+            if (parent_type == PTYPE_GET_TREE) :
                 var = {k:var}
+                log(LOG_DEBUG , "Parent get tree , var : " + str(var) , tid="100ay")
+
         elif ctx.match_b() != None :
-            var = (ctx.match_b().accept(self))
+            retCode , var = (ctx.match_b().accept(self))
             success = True
         elif (ctx.M() != None) :
             log(LOG_DEBUG , "m cand in all members") 
@@ -334,35 +349,40 @@ class customVisitor(test_1Visitor):
             # for k,v in parent.items():
                 # var.append()
         log(LOG_DEBUG , "Returning var : " + str(var) + " , success : " + str(success)  ,tid="587uj")
-        return success, var
+        if success :
+            retCode = RET_SUCCESS
+        return retCode, var
+
 
     def visitExpr(self, ctx: test_1Parser.ExprContext):
         self.commonVisitor(ctx , "expr")
         log(LOG_DEBUG , "In visitExpr." , tid="100ag")
         ans = ""
+        retCode = RET_FAILURE
         if ctx.math_b_op() != None :
             # Implies, there is num/bracket + expr
-            op = ctx.math_b_op().accept(self)
-            success , v2 = ctx.expr().accept(self)
+            retCode , op = ctx.math_b_op().accept(self)
+            retCode , v2 = ctx.expr().accept(self)
             if ctx.expr_1 != None:
-                v1 = ctx.expr_1().accept(self)
+                retCode, v1 = ctx.expr_1().accept(self)
             else:
                 log(LOG_WARNING , "Unexpected condition. " , tid="100aa")
-            success , ans = self.elementWiseOp(v1 , v2 , op)
+            retCode , ans = self.elementWiseOp(v1 , v2 , op)
         else:
             log(LOG_WARNING , "expression without operator. i.e. simple uid.")
             if ctx.expr_1 != None :
-                ans = ctx.expr_1().accept(self)
+                retCode, ans = ctx.expr_1().accept(self)
         
-        return True , ans
+        return retCode , ans
 
     def elementWiseOp(self, a , b , op) :
         c = {}
+        retCode = RET_FAILURE
         log(LOG_DEBUG , "op : "  +str(op) + " a : " + str(a) +" ,  b : " + str(b) + " , c : "+ str(c)\
             + " , type a : "  +str(type(a)) + " , type b : " + str(type(b)) ,tid="100aj"  )
         if (type(a) != dict) or (type(b) != dict) :
             log(LOG_DEBUG , "non dict received in elementWiseOp. " , tid="100ab")
-            return False , ""
+            return RET_FAILURE , ""
         for k in a.keys():
             log(LOG_DEBUG , "Checking key : " + str(k) , tid="100ai")
             if (k in b):
@@ -395,16 +415,16 @@ class customVisitor(test_1Visitor):
                     log(LOG_WARNING , "Nodes are leaf and non-leaf, Assigning one random node without operation." , tid="100ad")
                     c[k] = b[k]
                 else:
-                    success , c[k] = self.elementWiseOp(a[k] , b[k] , op)
+                    retCode , c[k] = self.elementWiseOp(a[k] , b[k] , op)
         log(LOG_DEBUG , "op : "  +str(op) + " a : " + str(a) +" ,  b : " + str(b) + " , c : "+ str(c),tid="100ae"  )
-        return True, c;
+        return retCode, c;
 
 
     def visitId_(self, ctx: test_1Parser.Id_Context , parent_type=DEFAULT):
         # self.commonVisitor(ctx, "id")
         text = str(ctx.ID())
         retCode , var = getVar(text)
-        if not present : 
+        if retCode == RET_FAILURE : 
             log(LOG_WARNING , "variable not in map : " + text)
         return retCode , var
         
@@ -435,10 +455,12 @@ class customVisitor(test_1Visitor):
         return RET_SUCCESS, False
 
 
-    def evalMember(self , ctx  , memberList , parent = {} , depth = 0):
+    def evalMember(self , ctx  , memberList , parent = {} , ptype = PTYPE_DEFAULT , depth = 0):
         v = {}
         success = True
         success1 = False
+        retCode = RET_FAILURE
+        ret1 = RET_SUCCESS
         log(LOG_DEBUG , " In evalMember , PARENT : " + str(parent) + " , memberList : " +str(memberList[0].getText()) )
         if len(memberList) > 1:
             log(LOG_DEBUG , "Going down a member..") 
@@ -446,40 +468,50 @@ class customVisitor(test_1Visitor):
                 log(LOG_DEBUG , "Encountered a * .")
                 for key_each in parent.keys() :
                     success = True
-                    success1 , val =  self.evalMember(ctx , memberList[1:] , parent[key_each] , depth = (depth +1)) 
-                    success = success and success1
+                    retCode , val =  self.evalMember(ctx , memberList[1:] , parent[key_each] , depth = (depth +1)) 
                     log(LOG_DEBUG , "success : " + str(success) + " , val : " +str(val) , tid="254dc"  )
-                    if (success) :
-                        v[key_each] = val
+                    if (retCode == RET_SUCCESS) :
+                        if ptype == PTYPE_DEFAULT:
+                            v = val
+                        elif ptype == PTYPE_GET_TREE :
+                            v[key_each] = val
+                        else:
+                            log(LOG_WARNING , "Uidentified ptype" , tid="100aw")
                     log(LOG_DEBUG , "v : " + str(v) , tid="465kn")
             elif memberList[0].uid() :
-                varName = memberList[0].uid().getText()
-                present , var = getVar(varName)
-                if present :
-                    if var in parent :
-                        success = True
-                        v = {}
-                        v[var] = parent[var]
+                retCode , var = memberList[0].uid().accept(self)
+                log(LOG_DEBUG , "var : "+ str(var) , tid="100an")
+                retCode , val = self.evalMember(ctx , memberList[1:] , parent[var] , depth=(depth + 1) )
+                log(LOG_DEBUG , "retcode : "+str(retCode) + " , val:" + str(val) , tid="100au")
+                if retCode == RET_SUCCESS :
+                    if ptype == PTYPE_DEFAULT : 
+                        v = val
+                    elif ptype == PTYPE_GET_TREE :
+                        v[var] = val
                     else : 
-                        log(LOG_ERROR , "ERR! var : " +(var) + " , parent : " +str(parent)   )
-                        return False, {}
-                success = True
+                        log(LOG_DEBUG , "Unidentified ptype" , tid="100ax")
+                else:
+                    log(LOG_DEBUG , "" , tid="100av")
+                    retCode = RET_FAILURE 
+                    v = {}
+                log(LOG_DEBUG , "v : " + str(v) , tid="100ao")
             elif ( memberList[0].match_b() ) : 
                 log(LOG_WARNING , "match_b not implemented in evalmembers..")
             else : 
                 log(LOG_WARNING , "Unconsidered member encountered. " , tid="100al")
         else : 
             log(LOG_DEBUG , "Reached terminal member of member candidates ." + " , memberList : " +str(memberList[0].getText()))
-            success1 , v = self.visitMember_candidate( memberList[0] , parent_type=PTYPE_MEMBER , parent = parent )
-            success = success and success1
-        log(LOG_DEBUG , "evalMember , PARENT : " + str(parent)+ " , v : " + str(v) + " , success : " + str(success) , tid="976hv")
-        return success, v
+            retCode , v = self.visitMember_candidate( memberList[0] , parent_type=PTYPE_MEMBER , parent = parent )
+        log(LOG_DEBUG , "evalMember , PARENT : " + str(parent)+ " , v : " + str(v) + " , retCode : " + str(retCode) , tid="976hv")
+        log(LOG_DEBUG , "" , tid="100as")
+
+        return retCode, v
 
 
     def commonVisitor(self, ctx  , ruleName):
         abc = ctx.getText()
         count = ctx.getChildCount()
-        print("visitor  :" + ruleName +" : " + abc + " ,  child count : " + str(count)  )        
+        log( LOG_DEBUG , "visitor  :" + ruleName +" : " + abc + " ,  child count : " + str(count)  , tid = "099aa" )        
         try:
             if (count > 0) : 
                 for i in range(0,count) : 
@@ -488,7 +520,7 @@ class customVisitor(test_1Visitor):
                     # print("done with " + str(c.getText()) )
 
             else : 
-                print("This is termilnal node..")
+                log(LOG_DEBUG , "This is termilnal node.." , tid="099ab")
         except:
             log(LOG_ERROR , "Exception in common visitor..")
 
@@ -501,9 +533,9 @@ def isFunction(param):
 
 def getVar(param):
     if isVariable(param):
-        return True , gVarMap[param]
+        return RET_SUCCESS , gVarMap[param]
     else:
-        return False , None
+        return RET_FAILURE , None
 
 
 def isVariable(param):
